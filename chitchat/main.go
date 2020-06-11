617d1f1ad5113b943c293d5925a51040c79ccf8b
takakuda <kei.takakuda.0415@icloud.com>
2020-06-12 03:03:32 +0900

    feat: add index handler

--- chitchat/main.go
+++ chitchat/main.go
@@ -1,8 +1,20 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 )
+
+func index(w http.ResponseWriter, r *http.Request) {
+	files := []string{"templates/layout.html",
+		"templates/navbar.html",
+		"templates/index.html"}
+	templates := template.Must(template.ParseFiles(files...))
+	threads, err := data.Threads()
+	if err == nil {
+		templates.ExecuteTemplate(w, "layout", threads)
+	}
+}
 
 func main() {
 	mux := http.NewServeMux()
