02154c99331302b37273228f5562428a32f12d07
takakuda <kei.takakuda.0415@icloud.com>
2020-06-19 03:06:16 +0900

    fix

--- chitchat/route_auth.go
+++ chitchat/route_auth.go
@@ -6,8 +6,7 @@ func authenticate(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	user, _ := data.UserByEmail(r.PostFormValue("email"))
 	if user.Password == data.Encrypt(r.PostFormValue("password")) {
-	session:
-		-user.CreateSession()
+		session := user.CreateSession()
 		cookie := http.Cookie{
 			Name:     "_cookie",
 			Value:    session.Uuid,
