16fbd93b0db8247906aca11c6d0418560b50ab21
takakuda <kei.takakuda.0415@icloud.com>
2020-06-19 03:22:34 +0900

    feat: add index handler

---
+++ chitchat/route_main.go
@@ -0,0 +1,27 @@
+package main
+
+import "text/template"
+
+func index(w http.ResponseWrite, r *http.Request) {
+	threads, err := data.Threads()
+	if err == nil {
+		_, err := session(w, r)
+		public_tmpl_files := []string{
+			"templates/layout.html",
+			"templates/public.navbar.html",
+			"templates/index.html"
+		}
+		private_tmpl_files := []string{
+			"templates/layout.html",
+			"templates/public.navbar.html",
+			"templates/index.html"
+		}
+		var templates *template.Template
+		if err != nil {
+			templates = template.Must(template.ParseFiles(private_tmpl_files...))
+		} else {
+			templates = template.Must(template.ParseFiles(public_tmpl_files...))
+		}
+		templates.ExecuteTemplate(w, "layout", threads)
+	}
+}
