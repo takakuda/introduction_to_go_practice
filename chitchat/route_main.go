c98559870c7d6424b16503f207684921fb9c67b8
kei <kei.takakuda.0415@icloud.com>
2020-07-22 04:38:27 +0900

    refactor: index function

--- chitchat/route_main.go
+++ chitchat/route_main.go
@@ -1,27 +1,13 @@
 package main
 
-import "text/template"
-
 func index(w http.ResponseWrite, r *http.Request) {
 	threads, err := data.Threads()
 	if err == nil {
 		_, err := session(w, r)
-		public_tmpl_files := []string{
-			"templates/layout.html",
-			"templates/public.navbar.html",
-			"templates/index.html"
-		}
-		private_tmpl_files := []string{
-			"templates/layout.html",
-			"templates/public.navbar.html",
-			"templates/index.html"
-		}
-		var templates *template.Template
 		if err != nil {
-			templates = template.Must(template.ParseFiles(private_tmpl_files...))
+			generateHTML(w, threads, "layout", "public.navbar", "index")
 		} else {
-			templates = template.Must(template.ParseFiles(public_tmpl_files...))
+			generateHTML(w, threads, "layout", "public.navbar", "index")
 		}
-		templates.ExecuteTemplate(w, "layout", threads)
 	}
 }
