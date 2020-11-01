2fe37941922511e2f4b1dc540f9080f267fcc3dd
takakuda <kei.takakuda.0415@icloud.com>
2020-11-02 07:51:53 +0900

    post

---
+++ src/first_webapp/data/main.go
@@ -0,0 +1,15 @@
+package main
+
+type Post struct {
+	Id      int
+	Content string
+	Author  string
+}
+
+var PostById map[int]*Post
+var PostsByAuthor map[string][]*Post
+
+func store(post Post) {
+	PostById[post.Id] = &post
+	PostsByAuthor[post.Author] = append(PostsByAuthor[post.Author], &post)
+}
