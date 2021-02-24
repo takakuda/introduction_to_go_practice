c17b2ca900b43e915c22d0d1613c4fc5be6320e7
kei <kei.takakuda.0415@icloud.com>
2021-02-25 04:34:11 +0900

    xml.go

---
+++ ch07/07/xml.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+)
+
+type Post struct {
+	XMLName xml.Name `xml:"post"`
+	Id      string   `xml:"id"`
+	Content string   `xml:"content"`
+	Author  Author   `xml:"author"`
+}
+
+type Author struct {
+	Id   string `xml:"id,attr"`
+	Name string `xml:",chardata"`
+}
+
+func main() {
+	post := Post{
+		Id:      "1",
+		Content: "Hello world!",
+		Author: Author{
+			Id:   "2",
+			Name: "takkkkkk",
+		},
+	}
+	output, err := xml.MarshalIndent(&post, "", "\t")
+	if err != nil {
+		fmt.Println("Error marhalling to XML:", err)
+		return
+	}
+	err = ioutil.WriteFile("post.xml", []byte(xml.Header+string(output)), 0644)
+	if err != nil {
+		fmt.Println("Error writing XML to file:", err)
+		return
+	}
+}
